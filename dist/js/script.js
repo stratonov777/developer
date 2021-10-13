725a68c408b140942fe9a55f1c4424993aa9d84a
Dmity <stratonov2017@gmail.com>
2021-10-13 20:48:05 +0500

    add preloader scroling section1

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close');
-
+      menuLink = document.querySelector('.menu__list'),
+      closeElem = document.querySelector('.menu__close'),
+      overlay = document.querySelector('.menu__overlay');
+      
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
@@ -10,9 +12,65 @@ closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
-const counters = document.querySelectorAll('.skills__ratings-counter'),
-      lines = document.querySelectorAll('.skills__ratings-line span');
+overlay.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+menuLink.addEventListener('click', () => {
+    // menu.classList.remove('active');
+});
+
+const counters = document.querySelectorAll('.diagram__unit-percent'),
+      lines = document.querySelectorAll('.diagram__unit-scaleActive');
 
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+    // Smooth scroll and pageup
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+});
+
+if (window.innerWidth > 760) {
+    $(function(){
+        $("a[href^='#']").click(function(){
+                var _href = $(this).attr("href");
+                $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+                menu.classList.remove('active');
+                return false;
+        });
+    });
+} else {
+    menuLink.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
+}
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+
+        $('form').trigger('reset');
+    });
+    return false;
+});
+
+new WOW().init(
+    {
+        boxClass:     'wow',      
+        animateClass: 'animated', 
+        offset:       0,          
+        mobile:       false,      
+        live:         true        
+      }
+);
